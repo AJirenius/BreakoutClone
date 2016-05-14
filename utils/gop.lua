3e6bb3e09c5d1a07c849f424876a5dcc403c634e
Andreas Jirenius <andreas.jirenius@king.com>
2016-05-14 11:44:37 +0200

    end_commit

--- utils/gop.lua
+++ utils/gop.lua
@@ -4,9 +4,10 @@ local gop_tables = {}
 
 function M.get_properties(url)
 	url = url or msg.url(".")
-	if type(url) ~= "string" then url = tostring(url) end
-	if gop_tables[url] == nil then gop_tables[url] = {} end
-	return gop_tables[url]
+	if type(url) == "string" then url = msg.url(url) end
+	msg.post("/main#gui","log",{str = url})
+	if gop_tables[url.path] == nil then gop_tables[url.path] = {} end
+	return gop_tables[url.path]
 end
 
 return M
